59db0e46f617fa2d6003fb3629c183af09a372ae
Wybe Westra <dev@wwestra.nl>
2020-11-01 15:19:25 +0100

    + Added save file format. And dictionary key convention.

--- scenes/file_menu.gd
+++ scenes/file_menu.gd
@@ -3,9 +3,9 @@ extends MenuButton
 signal new_galaxy_pressed
 signal load_galaxy_pressed
 
-enum {
-	NEW_GALAXY_ID,
-	LOAD_GALAXY_ID
+enum Id {
+	NEW_GALAXY,
+	LOAD_GALAXY
 }
 
 
@@ -13,14 +13,14 @@ enum {
 func _ready():
 	var popup := get_popup()
 	
-	popup.add_item("New Galaxy", NEW_GALAXY_ID)
-	popup.add_item("Open Galaxy", LOAD_GALAXY_ID)
+	popup.add_item("New Galaxy", Id.NEW_GALAXY)
+	popup.add_item("Open Galaxy", Id.LOAD_GALAXY)
 	
 	popup.connect("id_pressed", self, "_on_id_pressed")
 
 
 func _on_id_pressed(id: int):
-	if id == NEW_GALAXY_ID:
+	if id == Id.NEW_GALAXY:
 		emit_signal("new_galaxy_pressed")
-	elif id == LOAD_GALAXY_ID:
+	elif id == Id.LOAD_GALAXY:
 		emit_signal("load_galaxy_pressed")
